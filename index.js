a2db5929cd765dcc55bc40a6bd468218c8ba0b35
closedwall <yadavrajesh5612@gmail.com>
2023-10-23 23:23:09 +0530

    added start to package.json

--- index.js
+++ index.js
@@ -97,10 +97,7 @@ app.post("/delete",async(req,res)=>{
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
 
 
 app.listen(port,(req,res)=>{
